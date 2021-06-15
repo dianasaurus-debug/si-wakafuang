a8f023573a981438207f0c0e406482e3877c8f39
Diana Fitri <diana.fitri@uisi.ac.id>
2021-06-15 11:33:54 +0700

    too many commits

--- src/components/user/layouts/elements/Navbar2.vue
+++ src/components/user/layouts/elements/Navbar2.vue
@@ -4,7 +4,7 @@
     <!-- Left navbar links -->
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" data-toggle="push-menu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        <a class="nav-link" href="#" data-widget="pushmenu" role="button"><i class="fas fa-bars"></i></a>
       </li>
     </ul>
 
